ffc07628ac7b4dec8da4695b29b3df22cbd1271c
jaditi930 <jaditi930@gmail.com>
2025-10-22 20:29:20 +0530

    added vite-plugin-css-injected-by-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
   plugins: [
@@ -8,6 +9,7 @@ export default defineConfig({
     dts({
       insertTypesEntry: true, // <-- generates types entry
     }),
+    cssInjectedByJsPlugin()
   ],
   build: {
     lib: {
