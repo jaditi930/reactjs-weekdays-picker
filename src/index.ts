1dd9f0437ec98d76e94d4bce6f2539d14477070e
jaditi930 <jaditi930@gmail.com>
2025-10-22 18:29:30 +0530

    fixed imports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-export { default as DropdownSelector } from "./components/DropdownSelector";
+export { default as DropdownSelector } from "./components/DropdownSelector/index";
 export type { DropdownSelectorProps } from "./components/DropdownSelector/types";
 
-export { default as CustomMenuSelector } from "./components/CustomMenuSelector";
+export { default as CustomMenuSelector } from "./components/CustomMenuSelector/index";
 export type { CustomMenuSelectorProps } from "./components/CustomMenuSelector/types";
 
-export { default as CircularSelector } from "./components/CircularSelector";
+export { default as CircularSelector } from "./components/CircularSelector/index";
 export type { CircularSelectorProps } from "./components/CircularSelector/types";
 
-export { default as DefaultSelector } from "./components/DefaultSelector";
+export { default as DefaultSelector } from "./components/DefaultSelector/index";
 export type { DefaultSelectorProps } from "./components/DefaultSelector/types";
